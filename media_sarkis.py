2df68a237afb2da7a05b0cc88f70d7266f17466d
Ta-DevSark <tadevosian.sarkis@gmail.com>
2023-08-01 12:29:30 +0200

    "Find function works"

--- media_sarkis.py
+++ media_sarkis.py
@@ -1,14 +1,9 @@
-
+# -----------------------------------
 # pip install --upgrade pip
 # git clone nat's git 
 # git switch Sarkis
 # virtualenv venvmedia
 # source venvmedia/bin/activate
 # pip install pandas numpy matplotlib pymongo
-
-
-import pymongo
-
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-
-mydb = myclient["mydatabase"]
+# pip install -U nltk
+# -----------------------------------
